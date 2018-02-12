67508c821bcc433abc83f536516878d10fb7584c
LSchlegel <ludivine.sylvie@hotmail.fr>
2018-02-12 16:25:13 +0100

    exercice 1

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -3,7 +3,9 @@ import spark_helpers.SparkSessionHelper
 object Main {
   def main(args: Array[String]): Unit = {
     val spark = SparkSessionHelper.buildSession()
-    val sparkVersion = spark.version
-    println(s"Spark Version: $sparkVersion")
+    /*val sparkVersion = spark.version
+    println(s"Spark Version: $sparkVersion")*/
+
+    sql_practice.examples.exec1()
   }
 }
--- src/main/scala/sql_practice/examples.scala
+++ src/main/scala/sql_practice/examples.scala
@@ -1,5 +1,36 @@
 package sql_practice
 
-object examples {
+import org.apache.spark.sql.functions.{avg, explode, max, min}
+import spark_helpers.SparkSessionHelper
 
+import scala.collection.mutable
+
+
+object examples {
+  def exec1(): Unit ={
+    val spark = SparkSessionHelper.getSparkSession()
+    import spark.implicits._
+    val tourDF = spark.read.option("multiline", true).option("mode", "PERMISSIVE").json("data/input/tours.json")
+
+    println(tourDF.count())
+    //tourDF.show()
+    tourDF.printSchema()
+
+    val a = tourDF.groupBy("tourDifficulty").count()
+    //a.show()
+    println("unique levels of difficulty : "+a.count())
+
+    tourDF.agg(min("tourPrice"), max("tourPrice"), avg("tourPrice")).show()
+    tourDF.groupBy("tourDifficulty").agg(min("tourPrice"), max("tourPrice"), avg("tourPrice")).show()
+
+    tourDF.groupBy("tourDifficulty").agg(min("tourPrice"), max("tourPrice"), avg("tourPrice"), min("tourLength"), max("tourLength"), avg("tourLength")).show()
+
+
+    val b = tourDF.select(explode($"tourTags")).groupBy("col").count().sort($"count".desc)
+    b.show(10)
+    val c = tourDF.select(explode($"tourTags"), $"tourDifficulty").groupBy("col", "tourDifficulty").count().sort($"count".desc)
+    c.show(10)
+    val d = tourDF.select(explode($"tourTags"), $"tourDifficulty", $"tourPrice").groupBy("col", "tourDifficulty").agg(min("tourPrice"), max("tourPrice"), avg("tourPrice")).sort($"avg(tourPrice)".desc)
+    d.show(10)
+  }
 }
